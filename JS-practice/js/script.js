cd09a6820c161464e746f23a816f6ff5bb40d2f4
Andrew Wilsher <103464199+orelem@users.noreply.github.com>
2022-12-25 14:21:03 -0500

    Add two practice challenges to js

---
+++ JS-practice/js/script.js
@@ -0,0 +1,12 @@
+// Working with variables
+
+// let name = "John",
+//     admin = name;
+
+// alert(admin);
+
+
+// Giving the right name
+
+// let ourPlanetName = "Earth"
+// let currentUserName = "John"
