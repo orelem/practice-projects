13a53941f38f21bed6f73b808d919d4e7b70e5f5
Andrew Wilsher <103464199+orelem@users.noreply.github.com>
2022-12-28 17:22:54 -0500

    JS challenge: postfix and prefix forms

--- JS-practice/js/script.js
+++ JS-practice/js/script.js
@@ -1,3 +1,5 @@
+// Exercices from https://javascript.info/variables
+
 // Working with variables
 
 // let name = "John",
@@ -10,3 +12,29 @@
 
 // let ourPlanetName = "Earth"
 // let currentUserName = "John"
+
+
+// Uppercase const?
+
+// const BIRTHDAY = '18.04.1982'; // make birthday uppercase?
+
+// const AGE = someCode(BIRTHDAY); // make age uppercase?
+
+// We generally use upper case for constants that are “hard-coded”. Or, in other words, when the value is known prior to execution and directly written into the code.
+
+// In this code, birthday is exactly like that. So we could use the upper case for it.
+
+// In contrast, age is evaluated in run-time. Today we have one age, a year after we’ll have another one. It is constant in a sense that it does not change through the code execution. But it is a bit “less of a constant” than birthday: it is calculated, so we should keep the lower case for it.
+
+
+// Exercises from: https://javascript.info/operators
+
+// The postfix and prefix forms
+
+let a = 1, b = 1;
+
+let c = ++a; // 2
+let d = b++; // 1
+
+alert(c);
+alert(d);
