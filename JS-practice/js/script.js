ba57291b4e4c11d72ca2fc678918d2ce610b6e17
Andrew Wilsher <103464199+orelem@users.noreply.github.com>
2022-12-28 17:26:09 -0500

    Add explanation for postfix and prefix forms

--- JS-practice/js/script.js
+++ JS-practice/js/script.js
@@ -31,10 +31,11 @@
 
 // The postfix and prefix forms
 
-let a = 1, b = 1;
+// let a = 1, b = 1;
 
-let c = ++a; // 2
-let d = b++; // 1
+// let c = ++a; // 2
+// let d = b++; // 1
 
-alert(c);
-alert(d);
+// alert(c);
+// alert(d);
+// Explanation: ++a (prefix form) returns the new value while a++ (postfix form) returns the old value
