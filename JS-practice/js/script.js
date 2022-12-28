1079576ee4b704cc7c10359c981a7c366b4f576e
Andrew Wilsher <103464199+orelem@users.noreply.github.com>
2022-12-28 17:47:51 -0500

    Add JS Challenge: Type conversions

--- JS-practice/js/script.js
+++ JS-practice/js/script.js
@@ -45,8 +45,52 @@
 
 // Assignment result
 
-let a = 2;
+// let a = 2;
 
-let x = 1 + (a *= 2);
+// let x = 1 + (a *= 2); 
 
-alert(x);
+// a = 4 (multiplied by 2), x = 5 (calculated as 1 + 4)
+
+// alert(x);
+
+// Explanation: x = 5 because the value in the bracket gets calculated separately from what is out of the bracket kind of like the P(B).E.D.M.A.S. method in school
+
+// Type conversions
+
+// let a = "" + 1 + 0
+// let b ="" - 1 + 0
+// let c = true + false
+// let d = 6 / "3"
+// let e = "2" * "3"
+// let f = 4 + 5 + "px"
+// let g = "$" + 4 + 5
+// let h = "4" - 2
+// let i = "4px" - 2
+// let j = "  -9  " + 5
+// let k = "  -9  " - 5
+// let l = null + 1
+// let m = undefined + 1
+// let n = " \t \n" - 2
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+// console.log(i);
+// console.log(j);
+// console.log(k);
+// console.log(l);
+// console.log(m);
+// console.log(n);
+
+// 1. The addition with a string "" + 1 converts 1 to a string: "" + 1 = "1", and then we have "1" + 0, the same rule is applied.
+// 2. The subtraction - (like most math operations) only works with numbers, it converts an empty string "" to 0.
+// 3. The addition with a string appends the number 5 to the string.
+// 4. The subtraction always converts to numbers, so it makes " -9 " a number -9 (ignoring spaces around it).
+// 5. null becomes 0 after the numeric conversion.
+// 6. undefined becomes NaN after the numeric conversion.
+// 7. Space characters are trimmed off string start and end when a string is converted to a number. Here the whole string consists of space characters, such as \t, \n and a “regular” space between them. So, similarly to an empty string, it becomes 0.
